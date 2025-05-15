729a13a920bb84ad5c7ecc8f3f2e6a0df42fda20
cosmicchimp <max.lambert429@gmail.com>
2025-05-14 23:15:44 -0600

    Created a ton of middleware functions and implemented s3 and some JWT functions

--- backend/middleware/finduser.js
+++ backend/middleware/finduser.js
@@ -2,15 +2,15 @@ import { neon } from "@neondatabase/serverless";
 import bcrypt from "bcrypt"
 const sql = neon(process.env.DATABASE_URL);
 
-export default async function checkUser(user, password) {
+export default async function checkUser(user) {
   try {
     const result = await sql`SELECT * FROM userbase.users WHERE email = ${user.email}`;
     if (result.length === 0) {
       return false; // No user found
     }
-    console.log("Stored hash:", result[0].password, "password:", password);
+    console.log("Stored hash:", result[0].password, "password:", user.password);
     const cryptCheck = await bcrypt.compare(
-      password.trim(),
+      user.password.trim(),
       result[0].password.trim()
     );
     return cryptCheck;
